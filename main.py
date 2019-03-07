e414db24e50918f85e7251cfd5d98da6cdbeaced
Kanushka Gayan <gayan@extrogene.com>
2019-03-07 15:27:03 +0530

    add gcloud storage requiremnts

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 # @purpose: Upload file for recognize Sinhala text and give basic WEb view
 
 import os
+import cloudstorage as gcs
+import webapp2
+
+from google.appengine.api import app_identity
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 from logging.config import dictConfig
@@ -53,6 +57,7 @@ def upload_file():
             # file.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename))
             file.save('uploads/' + filename)
             app.logger.info('%s file uploaded successfully', filename)
+            app.logger.info(file)
 
             # get sinhala text form uploaded image
             text = sinhalaocr.convert_to_sinhala_text('uploads/' + filename)
@@ -70,3 +75,52 @@ if __name__ == '__main__':
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
+
+def get(self):
+  bucket_name = os.environ.get('sinhala-ocr-bucket',
+                               app_identity.get_default_gcs_bucket_name())
+
+  self.response.headers['Content-Type'] = 'text/plain'
+  self.response.write('Demo GCS Application running from Version: '
+                      + os.environ['CURRENT_VERSION_ID'] + '\n')
+  self.response.write('Using bucket name: ' + bucket_name + '\n\n')
+
+def create_file(self, filename):
+  """Create a file.
+
+  The retry_params specified in the open call will override the default
+  retry params for this particular file handle.
+
+  Args:
+    filename: filename.
+  """
+  self.response.write('Creating file %s\n' % filename)
+
+  write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+  gcs_file = gcs.open(filename,
+                      'w',
+                      content_type='text/plain',
+                      options={'x-goog-meta-foo': 'foo',
+                               'x-goog-meta-bar': 'bar'},
+                      retry_params=write_retry_params)
+  gcs_file.write('abcde\n')
+  gcs_file.write('f'*1024*4 + '\n')
+  gcs_file.close()
+  self.tmp_filenames_to_clean_up.append(filename)
+
+def read_file(self, filename):
+  self.response.write('Reading the full file contents:\n')
+
+  gcs_file = gcs.open(filename)
+  contents = gcs_file.read()
+  gcs_file.close()
+  self.response.write(contents)
+
+def delete_files(self):
+  self.response.write('Deleting files...\n')
+  for filename in self.tmp_filenames_to_clean_up:
+    self.response.write('Deleting file %s\n' % filename)
+    try:
+      gcs.delete(filename)
+    except gcs.NotFoundError:
+      pass
