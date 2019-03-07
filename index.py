397d06c69ae26ba7a672e258dfdddabffe63c42d
Kanushka Gayan <gayan@extrogene.com>
2019-03-07 13:49:22 +0530

    add app run command

--- index.py
+++ index.py
@@ -64,3 +64,9 @@ def upload_file():
             return render_template('upload-success.html', filename=filename, text=text)
 
     return render_template('upload.html')
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
