90588bd4debf888728e2dac712e67c1700a2561a
Kanushka Gayan <gayan@extrogene.com>
2019-03-08 16:06:59 +0530

    use pre processed image

--- sinhalaocr.py
+++ sinhalaocr.py
@@ -24,14 +24,14 @@ def convert_to_sinhala_text(filepath, scale=80):
 	gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]		
 
 	# make a check to see if median blurring should be done to remove noise
-	gray = cv2.medianBlur(gray, 3)
+	# gray = cv2.medianBlur(gray, 3)
 
 	# write the grayscale image to disk as a temporary file so we can apply OCR to it
 	filename = "{}.png".format(os.getpid())
 	cv2.imwrite(filename, gray)
 
 	# load the image as a PIL/Pillow image, apply OCR, and then delete the temporary file
-	text = pytesseract.image_to_string(Image.open(filename), lang='eng+sin')
+	text = pytesseract.image_to_string(gray, lang='eng+sin')
 	os.remove(filename)
 
 	return text
